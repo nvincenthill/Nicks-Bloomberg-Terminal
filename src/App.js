fe0cd97fdf3fee073ee62b0d92d4d3d390f1fc68
mypersonalgithubaccount <nvincenthill@gmail.com>
2018-05-17 15:24:31 -0700

    Remove boilerplate cruft

--- src/App.js
+++ src/App.js
@@ -1,18 +1,51 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+
+// first we will make a new context
+const MyContext = React.createContext();
+
+// Then create a provider Component
+class MyProvider extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+
+    // this.updateData = this.updateData.bind(this);
+  }
+
+  // get data
+  getData = async => {
+    // let query = this.state.value;
+    // fetch("https://api.scryfall.com/cards/named?" + "fuzzy=" + query)
+    //   .then(response => response.json())
+    //   .then(data => this.setState({ card: data }));
+  };
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  render() {
+    return (
+      <MyContext.Provider
+        value={{
+          state: this.state
+        }}
+      >
+        {this.props.children}
+      </MyContext.Provider>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Stock Quote Generator</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p className="App-intro">Hello World!</p>
       </div>
     );
   }
