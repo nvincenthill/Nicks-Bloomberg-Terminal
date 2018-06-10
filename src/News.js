b20e2aedcf4df31b087593af955833adcfc43698
mypersonalgithubaccount <nvincenthill@gmail.com>
2018-06-10 11:31:04 -0700

    Fix undefined URL bug in news

--- src/News.js
+++ src/News.js
@@ -6,16 +6,20 @@ class News extends React.Component {
   componentDidMount() {}
   componentDidUpdate() {}
   componentWillUnmount() {}
+  
+    //TO DO Refactor with iterator and variable
   render() {
     return (
       <MyContext.Consumer>
         {context => (
           <div className="quote-news">
+          {context.state.currentQuote.latestPrice ?
+            <React.Fragment>
             <h2 className="quote-subtitle">NEWS</h2>
             <ul className="quote-news-list">
               <a
                 href={
-                  context.state.currentQuote.latestPrice
+                  context.state.currentNews[0].url
                     ? context.state.currentNews[0].url
                     : null
                 }
@@ -27,65 +31,67 @@ class News extends React.Component {
               </a>
               <a
                 href={
-                  context.state.currentQuote.latestPrice
+                  context.state.currentNews[1]
                     ? context.state.currentNews[1].url
                     : null
                 }
               >
-              {context.state.currentQuote.latestPrice ?
+              {context.state.currentNews[1] ?
                 <li className="quote-news-title"> {context.state.currentNews[1].headline + " | "}
                 <span className="source">{context.state.currentNews[1].source}</span> 
                 </li> : null } 
               </a>
               <a
                 href={
-                  context.state.currentQuote.latestPrice
+                  context.state.currentNews[2]
                     ? context.state.currentNews[2].url
                     : null
                 }
               >
-              {context.state.currentQuote.latestPrice ?
+              {context.state.currentNews[2] ?
                 <li className="quote-news-title"> {context.state.currentNews[2].headline + " | "}
                 <span className="source">{context.state.currentNews[2].source}</span> 
                 </li> : null } 
               </a>
               <a
                 href={
-                  context.state.currentQuote.latestPrice
+                  context.state.currentNews[3]
                     ? context.state.currentNews[3].url
                     : null
                 }
               >
-              {context.state.currentQuote.latestPrice ?
+              {context.state.currentNews[3] ?
                 <li className="quote-news-title"> {context.state.currentNews[3].headline + " | "}
                 <span className="source">{context.state.currentNews[3].source}</span> 
                 </li> : null } 
               </a>
               <a
                 href={
-                  context.state.currentQuote.latestPrice
+                  context.state.currentNews[4]
                     ? context.state.currentNews[4].url
                     : null
                 }
               >
-              {context.state.currentQuote.latestPrice ?
+              {context.state.currentNews[4] ?
                 <li className="quote-news-title"> {context.state.currentNews[4].headline + " | "}
                 <span className="source">{context.state.currentNews[4].source}</span> 
                 </li> : null } 
               </a>
               <a
                 href={
-                  context.state.currentQuote.latestPrice
+                  context.state.currentNews[5]
                     ? context.state.currentNews[5].url
                     : null
                 }
               >
-              {context.state.currentQuote.latestPrice ?
+              {context.state.currentNews[5] ?
                 <li className="quote-news-title"> {context.state.currentNews[5].headline + " | "}
                 <span className="source">{context.state.currentNews[5].source}</span> 
                 </li> : null } 
               </a>
             </ul>
+            </React.Fragment>
+          : null}
           </div>
         )}
       </MyContext.Consumer>
