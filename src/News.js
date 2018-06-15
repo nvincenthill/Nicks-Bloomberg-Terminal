43f7339fe297d5565643679a73a9efaed5f7d6b3
mypersonalgithubaccount <nvincenthill@gmail.com>
2018-06-14 22:19:30 -0700

    Refactor news items

--- src/News.js
+++ src/News.js
@@ -10,15 +10,15 @@ class News extends React.Component {
   //TO DO Refactor with iterator and variable
   // handle HTML apostrophies and special characters
   render() {
-    const numbers = [0, 1, 2, 3, 4];
+    const numbers = [0, 1, 2, 3];
     let newsItems = numbers.map(number => (
       <MyContext.Consumer>
         {context =>
           context.state.currentQuote.latestPrice ? (
             <li className="quote-news-title">
-              {context.state.currentNews[numbers].headline + " | "}
+              {context.state.currentNews[number].headline + " | "}
               <span className="source">
-                {context.state.currentNews[numbers].source}
+                {context.state.currentNews[number].source}
               </span>
             </li>
           ) : null
@@ -33,108 +33,7 @@ class News extends React.Component {
               <React.Fragment>
                 <h2 className="quote-subtitle">NEWS</h2>
                 <ul className="quote-news-list">
-                  <a
-                    href={
-                      context.state.currentNews[0].url
-                        ? context.state.currentNews[0].url
-                        : null
-                    }
-                  >
-                    {context.state.currentQuote.latestPrice ? (
-                      <li className="quote-news-title">
-                        {" "}
-                        {context.state.currentNews[0].headline + " | "}
-                        <span className="source">
-                          {context.state.currentNews[0].source}
-                        </span>
-                      </li>
-                    ) : null}
-                  </a>
-                  <a
-                    href={
-                      context.state.currentNews[1]
-                        ? context.state.currentNews[1].url
-                        : null
-                    }
-                  >
-                    {context.state.currentNews[1] ? (
-                      <li className="quote-news-title">
-                        {" "}
-                        {context.state.currentNews[1].headline + " | "}
-                        <span className="source">
-                          {context.state.currentNews[1].source}
-                        </span>
-                      </li>
-                    ) : null}
-                  </a>
-                  <a
-                    href={
-                      context.state.currentNews[2]
-                        ? context.state.currentNews[2].url
-                        : null
-                    }
-                  >
-                    {context.state.currentNews[2] ? (
-                      <li className="quote-news-title">
-                        {" "}
-                        {context.state.currentNews[2].headline + " | "}
-                        <span className="source">
-                          {context.state.currentNews[2].source}
-                        </span>
-                      </li>
-                    ) : null}
-                  </a>
-                  <a
-                    href={
-                      context.state.currentNews[3]
-                        ? context.state.currentNews[3].url
-                        : null
-                    }
-                  >
-                    {context.state.currentNews[3] ? (
-                      <li className="quote-news-title">
-                        {" "}
-                        {context.state.currentNews[3].headline + " | "}
-                        <span className="source">
-                          {context.state.currentNews[3].source}
-                        </span>
-                      </li>
-                    ) : null}
-                  </a>
-                  <a
-                    href={
-                      context.state.currentNews[4]
-                        ? context.state.currentNews[4].url
-                        : null
-                    }
-                  >
-                    {context.state.currentNews[4] ? (
-                      <li className="quote-news-title">
-                        {" "}
-                        {context.state.currentNews[4].headline + " | "}
-                        <span className="source">
-                          {context.state.currentNews[4].source}
-                        </span>
-                      </li>
-                    ) : null}
-                  </a>
-                  <a
-                    href={
-                      context.state.currentNews[5]
-                        ? context.state.currentNews[5].url
-                        : null
-                    }
-                  >
-                    {context.state.currentNews[5] ? (
-                      <li className="quote-news-title">
-                        {" "}
-                        {context.state.currentNews[5].headline + " | "}
-                        <span className="source">
-                          {context.state.currentNews[5].source}
-                        </span>
-                      </li>
-                    ) : null}
-                  </a>
+                  {newsItems}
                 </ul>
               </React.Fragment>
             ) : null}
